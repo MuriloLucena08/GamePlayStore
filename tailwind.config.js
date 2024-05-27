594ce900a8cfd4539e99bb43bc000d7949c56c53
MuriloLucena08 <148287052+MuriloLucena08@users.noreply.github.com>
2024-05-27 20:02:27 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: ["./src/**/*.{html,js}", "./*.{html,js}"],
   theme: {
     screens:{
+      'mobile': '600px',
       'tablet': '640px',
       'laptop': '1024px',
       'desktop': '1280px',
